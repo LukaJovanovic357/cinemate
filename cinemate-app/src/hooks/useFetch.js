c6a775255a5657e62b7145be659a579bdace66dc
Luka <jovanovicluka276@outlook.com>
2025-03-14 12:09:14 +0200

    add apiPath for movieList page

--- cinemate-app/src/hooks/useFetch.js
+++ cinemate-app/src/hooks/useFetch.js
@@ -1,13 +1,20 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = url => {
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const useFetch = apiPath => {
     const [data, setData] = useState([]);
+
+    // https://api.themoviedb.org/3/movie/upcoming?api_key=f2c3eb3d87114d96edcbb244265d8b9b correct path
 
     useEffect(() => {
         const fetchMovies = async () => {
             try {
-                const response = await fetch(url);
+                const response = await fetch(
+                    `https://api.themoviedb.org/3${apiPath}?api_key=${API_KEY}`
+                );
                 const json = await response.json();
+
                 setData(json.results);
             } catch (error) {
                 console.log(error);
@@ -15,7 +22,7 @@ export const useFetch = url => {
         };
 
         fetchMovies();
-    }, []);
+    }, [data]);
 
     return {
         data
--- cinemate-app/src/pages/MovieList.jsx
+++ cinemate-app/src/pages/MovieList.jsx
@@ -1,12 +1,8 @@
 import Card from '../components/Card';
 import { useFetch } from '../hooks/useFetch.js';
 
-const API = import.meta.env.VITE_API_KEY;
-
-export const MovieList = () => {
-    const { data: movies } =
-        useFetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API}
-`);
+export const MovieList = ({ apiPath }) => {
+    const { data: movies } = useFetch(apiPath);
 
     return (
         <main>
